88bd7e1e0c2c43e9e85fbbce62c3a08f6bccc791
Aleksei Avitsuk <ky200782aav@gmail.com>
2026-03-15 22:30:22 +0100

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("org.jetbrains.intellij") version "1.17.4"
+    kotlin("jvm") version "2.0.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+group = "org.jetbrains"
+version = "1.0-SNAPSHOT"
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version.set("2024.1.4")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks {
+    patchPluginXml {
+        version.set("${project.version}")
+        changeNotes.set("""
+            Add change notes here.<br>
+            <em>most HTML tags may be used</em>        
+            """.trimIndent())
+    }
+}
