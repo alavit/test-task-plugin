aca1527fb685f36c8d63d390ef4273187856f0c2
Aleksei Avitsuk <ky200782aav@gmail.com>
2026-03-16 16:56:34 +0100

    add Task1

--- src/main/kotlin/org/jetbrains/TestTaskAction1.kt
+++ src/main/kotlin/org/jetbrains/TestTaskAction1.kt
@@ -3,19 +3,18 @@ package org.jetbrains
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.extensions.PluginId
+import com.intellij.ide.plugins.PluginManagerCore
 
 class TestTaskAction1 : AnAction() {
     override fun getActionUpdateThread() = ActionUpdateThread.BGT
 
-    override fun update(e: AnActionEvent) {
-        // Using the event, evaluate the context, and enable or disable the action.
-    }
-
     override fun actionPerformed(e: AnActionEvent) {
-        //Using the event, implement an action. For example, create and show a dialog.
-        val currentProject: Project = e.project!!
-        Messages.showMessageDialog(currentProject, "Here should be output of task 1", "Task 1", Messages.getInformationIcon())
+        val project = e.project ?: return
+        val version = PluginManagerCore.getPlugin(PluginId.getId("org.jetbrains.kotlin"))
+            ?.version
+            ?: "not found"
+        Messages.showMessageDialog(project, "Kotlin plugin version: $version", "Task 1", Messages.getInformationIcon())
     }
 }
